b667a96ce093a10cf9a2a738e74cb752a0b43cfd
deasyaulia <c050423002@mahasiswa.poliban.ac.id>
2024-12-27 16:46:10 +0800

    Menambahkan data

--- menu.cpp
+++ menu.cpp
@@ -1,78 +1,60 @@
 #include <iostream>
+#include <string>
 #include <conio.h>
 using namespace std;
-int data[100];
-int n;
 
+struct mahasiswa
+{
+    string nim;
+    string nama;
+    string alamat;
+    float ipk;
+};
 
+mahasiswa sikc[30];
 
-void masukkanData();
-void tampilkanData();
-void sortingAsc();
-void sortingDsc();
-void tukar(int *a, int *b);
+int pos = -1;
 
-void mPertama(string pesan){
-system("cls");
-cout<<"hallo saya menu "<<pesan;
-getch();
-
-}
-void tukar(int *a,int *b){
-int t=*a;
-*a=*b;
-*b=t;
+void dMenu()
+{
+    system("cls");
+    cout << "Aplikasi KelasQ" << "\n";
+    cout << "1. Masukkan data" << "\n";
+    cout << "2. Tampilkan data" << "\n";
+    cout << "3. Perbaikan data" << "\n";
+    cout << "4. Menghapus data" << "\n";
+    cout << "5. Exit" << "\n";
+    cout << "Masukan angka :";
 }
 
 int main()
 {
-  char pl;
-  cout << "Masukkan jumlah data (maksimal 100): ";
-  cin >> n; // Mengatur jumlah data di sini
-  if (n > 100) n = 100; // Batasi jumlah data maksimal 100
+    int pos = -1;
 
-  do
-  {
-    
-    system("cls");
-    cout << "Aplikasi sorting bubble" << "\n";
-    cout << "1. masukkan data" << "\n";
-    cout << "2. tampilkan data" << "\n";
-    cout << "3. sorting asc" << "\n";
-    cout << "4. sorting dsc" << "\n";
-    cout << "5. Exit" << "\n";
-    cout << "Masukan angka :";
-    pl = getch();
-    
-    switch (pl)
+    do
     {
-    case '1':
-      /* code */
-      masukkanData();
-      break;
-    case '2':
-      tampilkanData();
-      /* code */
-      break;
-    case '3':
-      sortingAsc();
-      /* code */
-      break;
-    case '4':
-      sortingDsc();
-      /* code */
-      break;
-    case '5':
-       cout << "Terima kasih.\n";
-      /* code */
-      break;
+        dMenu();
+        pl = getch();
 
-    default:
-      system("cls");
-      cout << "Pilihan Tidak Tersedia";
-      getch();
-      break;
-    }
-} while (pl != '5');
-  return 0;
+        switch (pl)
+        {
+        case '1':
+        {
+            pos++;
+            system("cls");
+            fflush(stdin);
+            cout << "masukan nim: ";
+            getline(cin, sikc[pos].nim);
+            cout << "masukan nama: ";
+            getline(cin, sikc[pos].nama);
+            cout << "masukan alamat: ";
+            getline(cin, sikc[pos].alamat);
+            cout << "masukan ipk: ";
+            cin >> sikc[pos].ipk;
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            break;
+    return 0;
+}
+ }
+ }
 }
